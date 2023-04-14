2b2c9cc073376984849d45b2491c3c0b5a4d455d
Kalaimani <kalaimani22raja@gmail.com>
2023-04-14 17:42:14 +0530

    Initial setup

---
+++ src/App.js
@@ -0,0 +1,19 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="header">
+        <a
+          className="logo"
+          href="/"
+          rel="noopener noreferrer"
+        >
+          Kalaimani
+        </a>
+      </header>
+    </div>
+  );
+}
+
+export default App;
