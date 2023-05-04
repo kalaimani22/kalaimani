4dac7467ded6cb49ddd8f5635887e4196331950e
Kalaimani <kalaimani22raja@gmail.com>
2023-05-04 08:31:22 +0530

    routing added

--- src/App.js
+++ src/App.js
@@ -1,7 +1,13 @@
 import './App.css';
+import { BrowserRouter as Router, Link, Route, Routes, Navigate} from 'react-router-dom';
+
+import Home from './pages/home';
+import About from './pages/about';
 
 function App() {
   return (
+    <Router>
+
     <div className="App">
       <header className="header">
         <a
@@ -11,8 +17,30 @@ function App() {
         >
           Kalaimani
         </a>
-      </header>
+
+        <nav>
+      <ul>
+        <li>
+          <Link to="home">Home</Link>
+        </li>
+        <li>
+          <Link to="about">about</Link>
+        </li>
+      </ul>
+    </nav>
+    </header>
+
+        <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="about" element={<About />} />
+        <Route
+      path="*"
+      element={<Navigate to="/" />}
+    />
+      </Routes>
     </div>
+    </Router>
+
   );
 }
 
