c8ad3993d1e21e2494e4c60e50fa5579ba4cb547
Kalaimani <kalaimani22raja@gmail.com>
2023-11-23 23:01:41 +0530

    Image slider added

--- src/pages/imageSlider.js
+++ src/pages/imageSlider.js
@@ -50,7 +50,7 @@ const ImageSlider = () => {
             key={index}
           >
             {index === current && (
-              <img src={slide.image} alt='travel image' className='image' />
+              <img src={slide.image} alt='travel' className='image' />
             )}
           </div>
         );
